bdde6850b72853392b5306154b875132ae83f6b7
“masum182” <masum.mollah@webable.digital>
2026-02-16 14:02:45 +0600

    build gradle add done

--- api/api_instance.js
+++ api/api_instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://ktobackend.etherstaging.xyz/api/v1",
+  baseURL: "https://api.kto.solutions/api/v1",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
